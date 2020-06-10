8a76466a4389a9e922ab2c87c9eae8471727179e
El-Kordy <Muhammadelekrady@gmail.com>
2020-06-10 02:37:08 +0200

    say besmallah before you open it

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-// Overlay
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+/* global $ , jQuery , alert , console , fadeIn */
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+$(function () {
+    
+    'use strict';
+
+    // ADD ACTIVE CLASS TO CLICK 
+    $('.base .base-post .post-case .heading button').click(function(){
+
+        $('.popup').fadeIn();
+    });
+    $('.popup').click(function(){
+        
+        $(this).fadeOut();
+    });
+    $('.popup .popup__content ').click(function(e){
+        
+        e.stopPropagation();
+    });
+
+    $('.popup .post-case-popup .exit button').click(function(){
+        $('.popup').fadeOut();
+    });
+
+
+    $('.cases-found__img-1').hover(function(){
+        $('.cases-found .box ').css("display","flex" ,function(){
+            $('this').fadeIn()
+        });
+    });
+
+});
